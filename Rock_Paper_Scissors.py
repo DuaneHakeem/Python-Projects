05e11e2ec31fa4e55a067af2379b7cd4b4df6391
Duane Stewart <154079274+DuaneHakeem@users.noreply.github.com>
2024-04-28 21:11:19 +0100

    SOLVED IMPORT ERROR

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -18,7 +18,7 @@ while True:
     computer_pick = options[random_number]
     print("Computer picked", computer_pick + ".")
 
-    if user_input == "rock" and computer_pick == "scissors"
+    if user_input == "rock" and computer_pick == "scissors":
     print("You won!")
     user_wins += 1
     continue
